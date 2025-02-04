23950941e7608303d23601ca5a5367e2873b5251
abrauf18 <abdul.rauf@ccript.com>
2025-02-04 21:48:55 +0500

    feat: integrate tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: [
+    './src/**/*.jsx',
+    './components/**/*.jsx',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
